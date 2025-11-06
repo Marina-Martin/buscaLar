93fca67b8ace4e6f0e24ed94b72f223f6252d519
Mariaki <mari.m4rtin@gmail.com>
2025-11-06 19:49:35 -0300

    unify footer and header

--- busca_lar/src/app/components/Header.js
+++ busca_lar/src/app/components/Header.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname(); 
   const mostrarPopup = () => {
     alert("Em construção"); 
   };
@@ -10,7 +12,12 @@ export default function Header() {
       <h1><Link href="/">Busca Lar</Link></h1>
       <nav>
         <ul>
-          <button className="btn" onClick={mostrarPopup}>Busca Pet</button>
+            <button className="btn" onClick={mostrarPopup}>Busca Pet</button>
+            {pathname !== "/forms" && (
+              <Link href="/forms">
+                <button>Busca Lar</button>
+              </Link>
+          )}
         </ul>
       </nav>
     </header>
